6a64c19f71c2c91bd5b7250c98396382cca0f829
Se4iv <beckron@mail.ru>
2020-09-14 23:49:49 +0300

    добавлено дз4

--- Homework/src/Homework42/SportCar.java
+++ Homework/src/Homework42/SportCar.java
@@ -1,5 +1,20 @@
 package Homework42;
 
 public class SportCar extends Car{
+    private String maxSpeed;
 
+    @Override
+    public void start() {
+        System.out.println(" SportCar поехал");
+    }
+
+    @Override
+    public void stop() {
+        System.out.println(" SportCar остановился");
+    }
+
+    @Override
+    public void printInfo() {
+        System.out.println(getType() + " " + getModel() + " " + getWeight() + " " + getEngine());
+    }
 }
